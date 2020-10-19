08679a27c744a42dac94e31d388e09adfc584f94
win-server-01 <shangjian2019@outlook.com>
2020-10-19 11:15:28 +0800

    登录注册完善，修改HTML可以获取后台返回内容

--- myweb/myapp/views.py
+++ myweb/myapp/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+
+from myapp import models
 from myapp.models import students
 
 
@@ -17,23 +19,31 @@ def addr(request):
     return render(request, 'addr.html')
 
 
+# 登录模块
 def login_action(request):
+    context = {}
     if request.method == "POST":
         user = request.POST.get('username', None)  # 避免提交为空时异常
         user = user.strip()  # 去除空格
         pwd = request.POST.get('password', None)
-        stuser = students.objects.get(student_user=user)
+        # 捕获查询不到数据异常，并进行处理
+        try:
+            stuser = students.objects.get(student_user=user)
+        except models.students.DoesNotExist:
+            context['usererror'] = '用户名不存在'
+            return render(request, 'login.html', context)
+
         studentpwd = str(stuser.student_password)
         if pwd == studentpwd:
-            print(user)
-            print(pwd)
             print('登录成功')
-            return render(request, 'user.html')
-
+            context['username'] = stuser
+            return render(request, 'user.html', context)
     print('登陆失败')
-    return render(request, 'login.html')
+    context['password'] = '密码错误，请重新输入'
+    return render(request, 'login.html', context)
 
 
+# 注册模块
 def insert(request):
     if request.method == "POST":
         add_user = request.POST.get('student_user')
